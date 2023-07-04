13d78149ab0b88e7166a86ce0c2821d17e575b11
user <kbedui0102@naver.com>
2023-07-05 00:01:49 +0900

    swagger

--- src/main/java/com/guro/GuroTestApplication.java
+++ src/main/java/com/guro/GuroTestApplication.java
@@ -3,7 +3,11 @@ package com.guro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.guro.config.SwaggerConfig;
+
+@Import(SwaggerConfig.class)
 @SpringBootApplication
 public class GuroTestApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/guro/ServerApplication.java
@@ -0,0 +1,11 @@
+package com.guro;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ServerApplication {
+
+}
